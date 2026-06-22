9c5e76907c6856fcd573d13e1f06b382b5ecf93b
Dmytro Hural <lakimon13@gmail.com>
2026-06-22 20:29:06 +0300

    Add wedding favicon assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#f4f0e8">
     <title>Оля + Діма | Запрошення</title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
